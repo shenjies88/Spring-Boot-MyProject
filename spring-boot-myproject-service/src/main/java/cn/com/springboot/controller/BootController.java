c8faa965239c00527a5a21554bb756caac75d1a3
shenjies88 <1961486254@qq.com>
2019-07-26 17:45:21 +0800

    add Boot

---
+++ spring-boot-myproject-service/src/main/java/cn/com/springboot/controller/BootController.java
@@ -0,0 +1,24 @@
+package cn.com.springboot.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author shenjies88
+ * @since 2019-07-26-17:34
+ */
+
+@Api(tags = "Boot")
+@RestController
+@RequestMapping("/boot")
+public class BootController {
+
+    @ApiOperation("Boot")
+    @GetMapping
+    public String Boot() {
+        return "Boot";
+    }
+}
