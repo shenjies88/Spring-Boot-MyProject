ca581dd5865bfab4cf513c4c262379419af2231a
shenjies88 <1961486254@qq.com>
2019-09-20 19:58:26 +0800

    fix

--- spring-boot-myproject-starter/src/main/java/cn/com/springboot/HelloWorldAutoConfiguration.java
+++ spring-boot-myproject-starter/src/main/java/cn/com/springboot/HelloWorldAutoConfiguration.java
@@ -2,7 +2,6 @@ package cn.com.springboot;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
  * @since 2019-05-09-14:42
  */
 @EnableConfigurationProperties(HelloWorldProperties.class)
+@ConditionalOnProperty(prefix = "hello-world", value = "enabled")
 @ConditionalOnClass(HelloWorld.class)
 @Configuration
 public class HelloWorldAutoConfiguration {
@@ -20,8 +20,6 @@ public class HelloWorldAutoConfiguration {
     @Autowired
     private HelloWorldProperties helloWorldProperties;
 
-    @ConditionalOnProperty(prefix = "hello-world", value = "enabled", havingValue = "true")
-    @ConditionalOnMissingBean(HelloWorld.class)
     @Bean
     public HelloWorld helloWorld() {
         return new HelloWorld(helloWorldProperties);
