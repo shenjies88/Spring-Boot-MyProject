de2bc5e3e0cac885d1827b33e1adde4dbeff5a2e
shenjies88 <1961486254@qq.com>
2020-02-04 11:43:08 +0800

    feat:新增jsr303异常校验

--- spring-boot-myproject-service/src/main/java/cn/com/springboot/common/ExceptionController.java
+++ spring-boot-myproject-service/src/main/java/cn/com/springboot/common/ExceptionController.java
@@ -3,24 +3,36 @@ package cn.com.springboot.common;
 import cn.com.springboot.HttpResult;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
- * 捕获controller层的异常
+ * 捕获异常
  */
+@ResponseStatus(HttpStatus.OK)
 @Slf4j
 @RestControllerAdvice("cn.com.springboot")
 public class ExceptionController {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public HttpResult handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        var className = e.getClass().getSimpleName();
+        var bindingResult = e.getBindingResult();
+        var errorMessage = new StringBuilder("参数校验失败: ");
+        bindingResult.getFieldErrors().forEach(fieldError -> errorMessage.append(fieldError.getDefaultMessage()).append(" "));
+        log.error(className, e);
+        return HttpResult.fail(errorMessage.toString());
+    }
+
     @ExceptionHandler(Exception.class)
-    public ResponseEntity exceptionHandler(Exception e) {
+    public HttpResult exceptionHandler(Exception e) {
         var message = e.getMessage();
         if (message == null) {
             message = e.getClass().getSimpleName();
         }
         log.warn(message, e);
-        return new ResponseEntity<>(HttpResult.fail(message), HttpStatus.OK);
+        return HttpResult.fail(message);
     }
 }
