9ad686898c86b58c74023d9a5e4704a08d09d9ab
shenjies88 <1961486254@qq.com>
2020-04-02 15:33:15 +0800

    fix

--- spring-boot-service/src/main/java/cn/com/springboot/jms/JmsConsumer.java
+++ spring-boot-service/src/main/java/cn/com/springboot/jms/JmsConsumer.java
@@ -1,14 +1,18 @@
 package cn.com.springboot.jms;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+/**
+ * @author shenjies88
+ */
 @Slf4j
 @Component
 public class JmsConsumer {
 
-    //        @JmsListener(destination = "logging.queue")
+    @JmsListener(destination = "logging.queue")
     public void consume(@Payload String message) {
         log.info("Consume: {}", message);
     }
