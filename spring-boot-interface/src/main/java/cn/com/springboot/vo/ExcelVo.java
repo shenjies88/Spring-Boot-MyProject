fa375a4570b0320e28981d4e24e943a4e0981891
shenjies88 <1961486254@qq.com>
2020-05-21 15:17:36 +0800

    上传下载功能

---
+++ spring-boot-interface/src/main/java/cn/com/springboot/vo/ExcelVo.java
@@ -0,0 +1,32 @@
+package cn.com.springboot.vo;
+
+import com.github.crab2died.annotation.ExcelField;
+import io.swagger.annotations.ApiModel;
+import lombok.Data;
+
+/**
+ * @author shenjies88
+ * @since 2020/5/21-2:13 PM
+ */
+@Data
+@ApiModel("Excel")
+public class ExcelVo {
+
+    /**
+     * 学号
+     */
+    @ExcelField(title = "学号", order = 1)
+    private Integer num;
+
+    /**
+     * 姓名
+     */
+    @ExcelField(title = "姓名", order = 2)
+    private String name;
+
+    /**
+     * 年龄
+     */
+    @ExcelField(title = "年龄", order = 3)
+    private Integer age;
+}
--- spring-boot-service/src/main/java/cn/com/springboot/controller/DownLoadController.java
+++ spring-boot-service/src/main/java/cn/com/springboot/controller/DownLoadController.java
@@ -1,21 +1,49 @@
 package cn.com.springboot.controller;
 
+import cn.com.springboot.vo.ExcelVo;
+import com.alibaba.fastjson.JSON;
+import com.github.crab2died.ExcelUtils;
+import com.github.crab2died.exceptions.Excel4JException;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
 import javax.servlet.http.HttpServletResponse;
-import java.io.UnsupportedEncodingException;
+import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 /**
  * @author shenjies88
  * @since 2019-04-16-15:52
  */
+@Slf4j
+@Api(tags = "上传下载")
+@RestController
 public class DownLoadController {
 
-    public void downLoad(HttpServletResponse response) throws UnsupportedEncodingException {
-
-        response.setContentType("application/x-download;charset=UTF-8");
-        response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode("文件名.xlsx", StandardCharsets.UTF_8));
-//        Files.copy(path, response.getOutputStream()); 写入输出流
-
+    @ApiOperation("上传下载")
+    @PostMapping("upload-download")
+    public void downLoad(@RequestParam(value = "file") MultipartFile file,
+                         HttpServletResponse response) {
+        try {
+            String fileName = file.getOriginalFilename();
+            List<ExcelVo> excelVos = ExcelUtils.getInstance().readExcel2Objects(file.getInputStream(), ExcelVo.class);
+            if (CollectionUtils.isEmpty(excelVos)) {
+                throw new RuntimeException("转换对象失败或无数据");
+            }
+            excelVos.forEach(e -> log.info(JSON.toJSONString(e)));
+            response.setHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(fileName, StandardCharsets.UTF_8));
+            response.setContentType("application/x-download;charset=UTF-8");
+            ExcelUtils.getInstance().exportObjects2Excel(excelVos, ExcelVo.class, response.getOutputStream());
+        } catch (IOException | Excel4JException e) {
+            log.info("读取文件流异常", e);
+        }
     }
 }
