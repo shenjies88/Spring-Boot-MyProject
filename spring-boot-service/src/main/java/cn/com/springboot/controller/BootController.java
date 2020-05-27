2a6d4beb5e1b3b8253aa7740cd501c65a449d4b8
shenjies88 <1961486254@qq.com>
2020-05-27 13:32:33 +0800

    webclient

--- spring-boot-service/src/main/java/cn/com/springboot/controller/BootController.java
+++ spring-boot-service/src/main/java/cn/com/springboot/controller/BootController.java
@@ -3,9 +3,12 @@ package cn.com.springboot.controller;
 import cn.com.springboot.HttpResult;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 /**
  * 例子boot
@@ -23,5 +26,19 @@ public class BootController {
     @GetMapping
     public HttpResult<String> boot() {
         return HttpResult.success("boot");
+    }
+
+    @ApiOperation("webClient")
+    @GetMapping("/web-client")
+    public HttpResult<String> webClient() {
+        WebClient client = WebClient.create("http://localhost:8080");
+        HttpResult<String> result = client.get()
+                .uri("/boot")
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<HttpResult<String>>() {
+                })
+                .block();
+        return result;
     }
 }
